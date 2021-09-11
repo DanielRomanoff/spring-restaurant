a3454e4b3f3fc679657cb6d75e1d3ada42e67c6f
Романов Даниил Николаевич <dnromanov@vtb.ru>
2021-09-11 11:30:05 +0300

    Init commit

---
+++ src/main/resources/changelog/changes/V1.0__init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `myschema`.`mytable`
+(
+    `id`   INT NOT NULL,
+    `name` VARCHAR(45) NULL,
+    PRIMARY KEY (`id`)
+);
---
+++ src/main/resources/changelog/changes/dev/V999.0__insert_data.sql
@@ -0,0 +1,3 @@
+/*
+INSERT INTO myschema.mytable(field1, field2) VALUES('value1', 'value2');
+ */
